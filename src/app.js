c6967fd802d26bab853d118d232325db5b7e14fa
Srinivas Bharadwaj H <srinivas.h@m2pfintech.com>
2024-11-17 18:05:08 +0530

    Added the route handlers

--- src/app.js
+++ src/app.js
@@ -2,7 +2,29 @@ const express = require('express');
 const app = express();
 
 
+
+app.get("/user",(req,res) => {
+    res.send({firstName: "Srinivas",lastName:"Bharadwaj"})
+})
+
+
+app.post("/user",(req,res) => {
+    res.send("User posted successfully")
+})
+
+
+app.delete("/user",(req,res) => {
+    res.send("User deleted successfully")
+})
+
+
+app.put("/user",(req,res) => {
+    res.send("User updated successfully")
+})
+
+
+
 //Listening on port
 app.listen(7777,() => {
-    console.log("Listening on port 7777")
+    console.log("Listeningg on port 7777")
 })
