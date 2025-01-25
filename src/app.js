92cb6c3a511a2e7db491b0d38dbdd6c50e427447
HSrinivasBharadwaj <srinivasbharadwaj.hullur@gmail.com>
2025-01-25 11:24:54 +0530

    Updated code

--- src/app.js
+++ src/app.js
@@ -2,25 +2,7 @@ const express = require('express');
 const app = express();
 const port = 7777;
 
-app.use("/user",(req,res) => {
-    res.send("Helllo")
-})
 
-app.get("/user",(req,res) => {
-    res.send("Hello srinivas")
-})
-
-app.post("/user",(req,res) => {
-    res.send("Data poste")
-})
-
-app.delete("/user",(req,res) => {
-    res.send("Data Deleted Successfully")
-})
-
-app.put("/user",(req,res) => {
-    res.send("Data updated successfully")
-})
 
 
 app.listen(port,() => {
