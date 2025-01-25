914d6d34299d4f1b2f6a596119de3b1a0a194af1
HSrinivasBharadwaj <srinivasbharadwaj.hullur@gmail.com>
2025-01-25 10:56:46 +0530

    Different Route Handlers scenrios

--- src/app.js
+++ src/app.js
@@ -2,8 +2,19 @@ const express = require('express');
 const app = express();
 const port = 7777;
 
-app.use("/hello",(req,res) => {
+
+
+app.use("/home",(req,res) => {
     res.send("Hello from server")
+})
+
+
+app.use("/home/123",(req,res) => {
+    res.send("Home route")
+})
+
+app.use("/",(req,res) => {
+    res.send("Hello from home route")
 })
 app.listen(port,() => {
     console.log("Listening on port " + port)
