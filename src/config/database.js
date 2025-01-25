8731b0cbae32b85c57141565bdd046419106350b
HSrinivasBharadwaj <srinivasbharadwaj.hullur@gmail.com>
2025-01-25 15:52:58 +0530

    Updated database name

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async() => {
     mongoose.connect(
-        "mongodb+srv://srinivasbharadwaj8:Hullur1234@cluster0.phr3z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        "mongodb+srv://srinivasbharadwaj8:Hullur1234@cluster0.phr3z.mongodb.net/devTinder"
       );
 }
 
