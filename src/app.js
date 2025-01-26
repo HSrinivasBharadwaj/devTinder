5afb88c34b38b55f983c443bfbbc242fd0bab5ce
HSrinivasBharadwaj <srinivasbharadwaj.hullur@gmail.com>
2025-01-26 10:41:52 +0530

    Added the http methods api

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const port = 7777;
 
 //Middleware for post request
 app.use(express.json())
+//Post Request
 app.post("/signup",async(req,res) => {
     //Reading the request
     const User = new user(req.body);
@@ -17,6 +18,69 @@ app.post("/signup",async(req,res) => {
         res.status(500).send("Error signing up user");
     })
 })
+
+
+//Get Request for all users
+app.get("/feed",async(req,res) => {
+    try {
+        const getAllUsers = await user.find({});
+        res.status(200).send(getAllUsers)
+    } catch (error) {
+        res.status(404).send("Not able to get the users")
+    }
+
+})
+
+//Get Request by Email Id
+app.get("/user",async(req,res) => {
+    const emailId = req.body.emailId;
+    try {
+        const getUserByEmail = await user.find({emailId: emailId});
+        return res.status(200).send(getUserByEmail)
+    } catch (error) {
+        res.status(401).send("Not able to find user by email")
+    }
+})
+
+//Delete By Id
+app.delete("/user",async(req,res) => {
+    const getUserId = req.body._id;
+    try {
+        const deleteUser = await user.findByIdAndDelete({_id: getUserId});
+        return res.status(200).send(deleteUser)
+    } catch (error) {
+        res.status(401).send("Not able to delete user")
+    }
+})
+
+//Delete All
+app.delete("/users",async(req,res) => {
+    try {
+        const deleteUsers = await user.deleteMany();
+        return res.status(200).send(deleteUsers)
+    } catch (error) {
+        res.status(401).send("Deleted All the users")
+    }
+})
+
+//Update By Id
+app.patch("/user",async(req,res) => {
+    const getUserId = req.body._id;
+    try {
+        const updateUser = await user.findByIdAndUpdate({_id:getUserId},{
+            firstName: "John",
+            lastName: "Doe",
+            emailId: "JohnDoe@email.com",
+            password: "testOne",
+            age: 21,
+            gender: "female"
+        })
+        return res.status(200).send(updateUser)
+    } catch (error) {
+        return res.status(404).send("Not able to update the user")
+    }
+})
+
 
 connectToDB().then(() => {
     console.log("Database connected successfully");
