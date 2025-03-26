6ab5ba9e084271f1c9994ea6cfd13204c86d5c30
HSrinivasBharadwaj <srinivasbharadwaj.hullur@gmail.com>
2025-03-26 21:04:27 +0530

    Final commit

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectDB = async() => {
-    mongoose.connect(
-        "mongodb+srv://srinivasbharadwaj8:Hullur1234@cluster0.phr3z.mongodb.net/devTinder"
-      );
-}
-
-module.exports = connectDB
