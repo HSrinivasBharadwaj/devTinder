bf0be959f88f8a2d63bf3c636af72f6244cee617
HSrinivasBharadwaj <srinivasbharadwaj.hullur@gmail.com>
2025-03-28 10:03:06 +0530

    Handled mutliple route handlers

--- src/app.js
+++ src/app.js
@@ -1,26 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Hello user");
+    next();
+  },
+  (req, res,next) => {
+    console.log("Hello from second route");
+    // res.send("Sending the response from second route handler");
+    next();
+  },
+  (req, res,next) => {
+    console.log("Hello from third route");
+    // res.send("Sending the response from third route handler");
+    next();
+  },
+  (req, res,next) => {
+    console.log("Hello from fourth route");
+    res.send("Sending the response from fourth route handler");
+  }
+);
 
-app.use("/",(req,res) => {
-    res.send("Hello from main route")
-})
-
-app.use("/test/123",(req,res) => {
-    res.send("Hello from test routes")
-})
-
-app.use("/test",(req,res) => {
-    res.send("Hello from test route")
-})
-
-app.use("/home",(req,res) => {
-    res.send("Hello from home route")
-})
-
-
-
-app.listen(port,() => {
-    console.log("Listening on port",port)
-})
+app.listen(port, () => {
+  console.log("Listening on port", port);
+});
