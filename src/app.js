88b84b08fc35e0c93fe1ad7aa10f79dce8679bb8
HSrinivasBharadwaj <srinivasbharadwaj.hullur@gmail.com>
2025-03-29 12:22:36 +0530

    Added the validations for the model and api request

--- src/app.js
+++ src/app.js
@@ -14,8 +14,60 @@ app.post("/signup",async(req,res) => {
         await user.save();
         res.status(200).send("User Added Successfully")
     } catch (error) {
-        res.status(500).send("Error while adding the data to the database")
+        res.status(500).send(error)
     } 
+})
+
+
+//Get all users from the database
+app.get("/feed",async(req,res) => {
+    const findUsers = await User.find({});
+    res.status(200).send(findUsers)
+})
+
+//Get user by emailId
+app.get("/user",async(req,res) => {
+    const emailId = req.body.email;
+    try {
+       const findUser = await User.findOne({email: emailId}) ;
+       if (findUser.length === 0) {
+            res.status(400).send("Not able to find the user")
+       }
+       res.status(200).send(findUser)
+    } catch (error) {
+        res.status(500).send("Not able to find the user")
+    }
+})
+
+//Delete By ID
+app.delete("/user",async(req,res) => {
+    const userId = req.body.userId;
+    try {
+        const deleteUser = await User.findByIdAndDelete({_id:userId});
+        res.status(200).send("User deleted Successfully")
+    } catch (error) {
+        res.status(500).send("Not able to delete the user")
+    }
+})
+
+//Update the data by Id
+app.patch("/user/:userId",async(req,res) => {
+    const userId = req.params?.userId;
+    const data = req.body;
+    try {
+        const ALLOWED_UPDATES = ["gender","age","skills","photoUrl"];
+        const allowUpdate = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k));
+        if (!allowUpdate) {
+            throw new Error("Update was not allowed")
+        }
+        if(data.skills.length >=10) {
+            throw new Error("Skills cant be more than 10")
+        }
+        await User.findByIdAndUpdate({_id:userId},data,{runValidators:true});
+        res.status(200).send("User Updated Successfully")
+    } catch (error) {
+        res.status(500).send("Not able to update the user")
+    }
 })
 
 connectDB().then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,58 @@
 const mongoose = require('mongoose');
 
+
 const userSchema = mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required:true,
+        minLength:1,
+        maxLength:18 
     },
     lastName: {
         type: String
     },
     email: {
-        type: String
+        type: String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true,
+        validate(value) {
+           const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+           if (!emailRegex.test(value)) {
+                throw new Error("Invalid Email Format")
+           } 
+        }    
     },
     gender: {
-        type: String
+        type: String,
+        lowercase:true,
+        //Custom Validation for genders;
+        validate(value) {
+            if (!["male","female"].includes(value)) {
+                throw new Error("Only Male and Female genders are allowed")
+            }
+        }
     },
     password: {
-        type: String
+        type: String,
+        required:true
     },
     age: {
-        type: Number
+        type: Number,
+        required:true,
+    },
+    about: {
+        type: String,
+        default: "Hey im developer I want to connect with you"
+    },
+    skills: {
+        type: [String],
+    },
+    photoUrl: {
+        type: String,
+        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtRs_rWILOMx5-v3aXwJu7LWUhnPceiKvvDg&s"
     }
-})
+},{timestamps:true})
 
 module.exports = mongoose.model("User",userSchema)
