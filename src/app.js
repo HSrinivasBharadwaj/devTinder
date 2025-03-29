8a35c1d428b11d10c5427040c9a500182ba0910c
HSrinivasBharadwaj <srinivasbharadwaj.hullur@gmail.com>
2025-03-29 15:27:15 +0530

    Added the bcrypt for login route

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/user");
 const bcrypt = require("bcrypt");
+const validator = require('validator');
 const { validateSignUpData } = require("./utils/validate");
 const port = 3000;
 
@@ -27,6 +28,37 @@ app.post("/signup", async (req, res) => {
     res.status(500).send(error);
   }
 });
+
+
+//Login api
+app.post("/login",async(req,res) => {
+    try {
+        const {email,password} = req.body;
+        if (!validator.isEmail(email)) {
+            throw new Error("Please Enter the valid email format")
+        }
+        else if(!validator.isStrongPassword(password)) {
+            throw new Error("Password must be at least 8 characters long and contain at least one lowercase letter, one uppercase letter, one number, and one special symbol ")
+        }
+        //Check if email exists in the database;
+        const isUser = await User.findOne({email:email});
+        if (!isUser) {
+            throw new Error("Email Not found, please Sign Up")
+        }
+        //Decrypt the password
+        const isPasswordCorrect = await bcrypt.compare(password,isUser.password);
+        if (isPasswordCorrect) {
+            res.status(200).send("Login was successful")
+        }
+        else {
+            res.status(500).send("Invalid Credentials, please try again later")
+        }
+    } catch (error) {
+        console.log("error",error);
+        res.status(500).send("Error while logging in")
+    }
+    
+})
 
 //Get all users from the database
 app.get("/feed", async (req, res) => {
