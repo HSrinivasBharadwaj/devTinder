ba01ff1fe925b51772aa7dcfd90f5badd2a5e3c7
HSrinivasBharadwaj <srinivasbharadwaj.hullur@gmail.com>
2025-03-30 11:23:07 +0530

    Added the json web token setup and validate the token in profile page

--- src/app.js
+++ src/app.js
@@ -3,11 +3,14 @@ const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/user");
 const bcrypt = require("bcrypt");
-const validator = require('validator');
+const validator = require("validator");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 const { validateSignUpData } = require("./utils/validate");
 const port = 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 //Post Request for adding the data to the database
 app.post("/signup", async (req, res) => {
@@ -29,36 +32,61 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-
 //Login api
-app.post("/login",async(req,res) => {
-    try {
-        const {email,password} = req.body;
-        if (!validator.isEmail(email)) {
-            throw new Error("Please Enter the valid email format")
-        }
-        else if(!validator.isStrongPassword(password)) {
-            throw new Error("Password must be at least 8 characters long and contain at least one lowercase letter, one uppercase letter, one number, and one special symbol ")
-        }
-        //Check if email exists in the database;
-        const isUser = await User.findOne({email:email});
-        if (!isUser) {
-            throw new Error("Email Not found, please Sign Up")
-        }
-        //Decrypt the password
-        const isPasswordCorrect = await bcrypt.compare(password,isUser.password);
-        if (isPasswordCorrect) {
-            res.status(200).send("Login was successful")
-        }
-        else {
-            res.status(500).send("Invalid Credentials, please try again later")
-        }
-    } catch (error) {
-        console.log("error",error);
-        res.status(500).send("Error while logging in")
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!validator.isEmail(email)) {
+      throw new Error("Please Enter the valid email format");
+    } else if (!validator.isStrongPassword(password)) {
+      throw new Error(
+        "Password must be at least 8 characters long and contain at least one lowercase letter, one uppercase letter, one number, and one special symbol "
+      );
     }
-    
-})
+    //Check if email exists in the database;
+    const isUser = await User.findOne({ email: email });
+    if (!isUser) {
+      throw new Error("Email Not found, please Sign Up");
+    }
+    //Decrypt the password
+    const isPasswordCorrect = await bcrypt.compare(password, isUser.password);
+    if (isPasswordCorrect) {
+      //Generating the json webtoken;
+      const token = await jwt.sign({ _id: isUser._id }, "Hullur9606@");
+      res.cookie("token", token);
+      res.status(200).send("Login was successful");
+    } else {
+      res.status(500).send("Invalid Credentials, please try again later");
+    }
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).send("Error while logging in");
+  }
+});
+
+//Profile Api
+app.get("/profile", async (req, res) => {
+  try {
+    //Read the token
+    const cookies = req.cookies;
+    const { token } = cookies;
+    if (!token) {
+      res.status(500).send("Token Not Found Please Login Again");
+    }
+    //Validate the token;
+    const decodedMessage = jwt.verify(token, "Hullur9606@");
+    //Extract the user Id
+    const { _id } = decodedMessage;
+    const getUser = await User.find({ _id });
+    if (!getUser) {
+      res.status(500).send("User Not Found, Please SignUp");
+    }
+    console.log("getUser", getUser);
+    res.status(200).send(getUser);
+  } catch (error) {
+    res.status(500).send("Failed to fetch the profile data of the user")
+  }
+});
 
 //Get all users from the database
 app.get("/feed", async (req, res) => {
