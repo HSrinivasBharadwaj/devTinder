7d4e0cd2e4e1355f94218e5550769aadca22937d
HSrinivasBharadwaj <srinivasbharadwaj.hullur@gmail.com>
2025-03-31 10:04:37 +0530

    Added the token logic to auth middleware

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const validator = require("validator");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const { validateSignUpData } = require("./utils/validate");
+const {validateAuth} = require('./middlewares/auth');
 const port = 3000;
 
 app.use(express.json());
@@ -52,96 +53,39 @@ app.post("/login", async (req, res) => {
     const isPasswordCorrect = await bcrypt.compare(password, isUser.password);
     if (isPasswordCorrect) {
       //Generating the json webtoken;
-      const token = await jwt.sign({ _id: isUser._id }, "Hullur9606@");
+      const token = await jwt.sign({ _id: isUser._id }, "Hullur9606@",{
+        expiresIn: "1h"
+      });
       res.cookie("token", token);
       res.status(200).send("Login was successful");
     } else {
       res.status(500).send("Invalid Credentials, please try again later");
     }
   } catch (error) {
-    console.log("error", error);
     res.status(500).send("Error while logging in");
   }
 });
 
 //Profile Api
-app.get("/profile", async (req, res) => {
+app.get("/profile",validateAuth, async (req, res) => {
   try {
     //Read the token
-    const cookies = req.cookies;
-    const { token } = cookies;
-    if (!token) {
-      res.status(500).send("Token Not Found Please Login Again");
-    }
-    //Validate the token;
-    const decodedMessage = jwt.verify(token, "Hullur9606@");
-    //Extract the user Id
-    const { _id } = decodedMessage;
-    const getUser = await User.find({ _id });
-    if (!getUser) {
-      res.status(500).send("User Not Found, Please SignUp");
-    }
-    console.log("getUser", getUser);
-    res.status(200).send(getUser);
+    const user = req.user;
+    return res.status(200).send(user)
+  } catch (error) {
+    return res.status(500).send("Failed to fetch the profile data of the user")
+  }
+});
+
+//Send Connection Request
+app.post("/sendConnectionRequest",validateAuth,async(req,res) => {
+  try {
+    const user = req.user;
+    return res.status(200).send(user.firstName + "Send connection successfully")
   } catch (error) {
     res.status(500).send("Failed to fetch the profile data of the user")
   }
-});
-
-//Get all users from the database
-app.get("/feed", async (req, res) => {
-  const findUsers = await User.find({});
-  res.status(200).send(findUsers);
-});
-
-//Get user by emailId
-app.get("/user", async (req, res) => {
-  const emailId = req.body.email;
-  try {
-    const findUser = await User.findOne({ email: emailId });
-    if (findUser.length === 0) {
-      res.status(400).send("Not able to find the user");
-    }
-    res.status(200).send(findUser);
-  } catch (error) {
-    res.status(500).send("Not able to find the user");
-  }
-});
-
-//Delete By ID
-app.delete("/user", async (req, res) => {
-  const userId = req.body.userId;
-  try {
-    const deleteUser = await User.findByIdAndDelete({ _id: userId });
-    res.status(200).send("User deleted Successfully");
-  } catch (error) {
-    res.status(500).send("Not able to delete the user");
-  }
-});
-
-//Update the data by Id
-app.patch("/user/:userId", async (req, res) => {
-  const userId = req.params?.userId;
-  const data = req.body;
-  try {
-    const ALLOWED_UPDATES = ["gender", "age", "skills", "photoUrl"];
-    const allowUpdate = Object.keys(data).every((k) =>
-      ALLOWED_UPDATES.includes(k)
-    );
-    if (!allowUpdate) {
-      throw new Error("Update was not allowed");
-    }
-    if (data.skills.length >= 10) {
-      throw new Error("Skills cant be more than 10");
-    }
-    await User.findByIdAndUpdate({ _id: userId }, data, {
-      runValidators: true,
-    });
-    res.status(200).send("User Updated Successfully");
-  } catch (error) {
-    res.status(500).send("Not able to update the user");
-  }
-});
+})
 
 connectDB()
   .then(() => {
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,12 +1,24 @@
-const isAdminAuth = (req,res,next) => {
-    const token = "xyzz";
-    const isAuthenticated = token === "xyz";
-    if (!isAuthenticated) {
-        res.status(401).send("Unauthorized")
-    }
-    else {
-        next()
-    }
-}
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
-module.exports = {isAdminAuth}
+const validateAuth = async(req, res, next) => {
+  try {
+    const cookies = req.cookies;
+    const { token } = cookies;
+    if (!token) {
+      throw new Error("Token is not Valid");
+    }
+    const decodedObj = jwt.verify(token, "Hullur9606@");
+    const {_id} = decodedObj;
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("User not found");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(500).send("Failed to authenticate the user");
+  }
+};
+
+module.exports = { validateAuth };
